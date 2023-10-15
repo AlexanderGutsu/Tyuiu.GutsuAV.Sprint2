94ca677d9a933126a452f1766b87da08f110eb58
Tyuiu.GutsuAV@outlook.com <gutsu05@mail.ru>
2023-10-15 20:45:13 +0300

    Task7

---
+++ Tyuiu.GutsuAV.Sprint2.Task7.V11/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GutsuAV.Sprint2.Task7.V11.Lib;
+
+namespace Tyuiu.GutsuAV.Sprint2.Task7.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Гуцу А. В. | ИИПБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Гуцу Александр Владимирович | ИИПБ-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите значение переменной x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение переменной y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (res)
+            {
+                Console.WriteLine("Точка находиться в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находиться в заштрихованной области");
+            }
+            Console.ReadKey();
+        }
+    }
+}

7b475bd1534982222fe786d4c99cebd7210ecc33
Tyuiu.GutsuAV@outlook.com <gutsu05@mail.ru>
2023-10-15 15:39:44 +0300

    Task1

--- Tyuiu.GutsuAV.Sprint2.Task1.V13/Program.cs
+++ Tyuiu.GutsuAV.Sprint2.Task1.V13/Program.cs
@@ -29,6 +29,36 @@ namespace Tyuiu.GutsuAV.Sprint2.Task1.V13
 
             res = ds.GetLogicOperations(a, b, c, d);
 
+            Console.Title = "Спринт #2 | Выполнил: Гуцу А. В. | ИИПБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Логические операции                                               *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Гуцу Александр Владимирович | ИИПБ-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране.                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
         }
     }
 }

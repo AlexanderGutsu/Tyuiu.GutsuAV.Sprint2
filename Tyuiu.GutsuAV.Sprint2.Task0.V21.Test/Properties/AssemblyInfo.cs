010c243e4dc410cb68594f6db8928aa52f99ac61
Tyuiu.GutsuAV@outlook.com <gutsu05@mail.ru>
2023-10-15 14:37:27 +0300

    Task0

---
+++ Tyuiu.GutsuAV.Sprint2.Task0.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GutsuAV.Sprint2.Task0.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GutsuAV.Sprint2.Task0.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("265a57d8-be01-4adc-afdc-2aaca86c6855")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
